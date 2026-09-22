ed73cca8550938584845d623ad0ba0406400c061
Prajwal Dahal <prajwaldahal072@gmail.com>
2026-09-22 14:28:40 +0545

    Expose booking endpoints

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
-from app.api.v1.endpoints import profile, slots
+from app.api.v1.endpoints import bookings, profile, slots
 
 api_router = APIRouter()
 api_router.include_router(profile.router)
 api_router.include_router(slots.router)
+api_router.include_router(bookings.router)
