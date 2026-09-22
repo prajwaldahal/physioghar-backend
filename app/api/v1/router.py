03a7881138994d4fb70f1d46643ac0cd8f56af79
Prajwal Dahal <prajwaldahal072@gmail.com>
2026-09-22 14:29:46 +0545

    Expose patient and note endpoints

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
-from app.api.v1.endpoints import bookings, profile, slots
+from app.api.v1.endpoints import bookings, patients, profile, slots
 
 api_router = APIRouter()
 api_router.include_router(profile.router)
 api_router.include_router(slots.router)
 api_router.include_router(bookings.router)
+api_router.include_router(patients.router)
